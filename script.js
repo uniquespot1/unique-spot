4c397a622b180b5ac6e1b0caf4626d4000a5754b
uniquespot1 <uniquespot132@gmail.com>
2026-02-21 21:53:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function order() {
-    window.open("https://wa.me/94776939483?text=Hello Unique Spot, I want to order this product");
+let products = [
+ {name:"iPhone 13", price:250000, image:"phone.jpg"},
+ {name:"Headset", price:5000, image:"headset.jpg"},
+ {name:"T-Shirt", price:3000, image:"shirt.jpg"}
+];
+
+let cart = [];
+
+function displayProducts(list){
+let html="";
+list.forEach((p,i)=>{
+html+=`
+<div class="card">
+<img src="${p.image}" width="150">
+<h3>${p.name}</h3>
+<p>Rs ${p.price}</p>
+<button onclick="addCart(${i})">Add to Cart</button>
+</div>
+`;
+});
+document.getElementById("products").innerHTML=html;
+}
+
+displayProducts(products);
+
+function searchProduct(){
+let value=document.getElementById("search").value.toLowerCase();
+let filtered=products.filter(p=>p.name.toLowerCase().includes(value));
+displayProducts(filtered);
+}
+
+function addCart(i){
+cart.push(products[i]);
+alert("Added to cart");
+}
+
+function openCart(){
+alert("Cart Items: "+cart.length);
 }
