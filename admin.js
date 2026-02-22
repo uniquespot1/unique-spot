3c1e98e245a7cd97aebbbf6c58dd9c26e63925b4
uniquespot1 <uniquespot132@gmail.com>
2026-02-22 09:25:44 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,2 @@
-let password = "admin123";
 
-function login(){
-let p=document.getElementById("pass").value;
-if(p===password){
-document.getElementById("adminPanel").style.display="block";
-}else{
-alert("Wrong password");
-}
-}
 
-function addProduct(){
-let name=document.getElementById("name").value;
-let price=document.getElementById("price").value;
-let image=document.getElementById("image").value;
-
-let product = {name,price,image};
-
-localStorage.setItem("newProduct", JSON.stringify(product));
-
-alert("Product Added");
-}
